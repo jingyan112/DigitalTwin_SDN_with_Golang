e39db00d09f20d4c42fb8c4f7644495459ad41da
JING YAN <jingyan27@outlook.com>
2022-03-31 18:53:45 +0200

    Modify some code

--- jingyan_go_exer/main.go
+++ jingyan_go_exer/main.go
@@ -1,180 +1,197 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"time"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/mount"
 	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
-	"main/activate_onos_app"
-	"main/generate_file4topo_create"
+	"github.com/docker/docker/api/types"
+	container "github.com/docker/docker/api/types/container"
 )
 
-func main() {
-	//
-	// Initialize a new API client to communicate with the docker daemon
-	// Print err msg and exit if the operation fails
-	//
+const OnosImageName = "registry.hub.docker.com/onosproject/onos"
+const OnosContainerName = "onos"
+const OnosFwdAppName = "org.onosproject.fwd"
+const OnosOpenflowName = "org.onosproject.openflow"
+
+const MininetImageName = "registry.hub.docker.com/iwaseyusuke/mininet"
+const MininetContainerName = "mininet"
+const HostVolMap = "/Users/yanjing/Desktop/jingyan_go_exer/mininet"
+const MininetVolMap = "/tmp"
+
+const TemplateTopologyFile = "/Users/yanjing/Desktop/jingyan_go_exer/topo_templates/create_tree_topo_template.sh"
+const NewTopologyFile = "/Users/yanjing/Desktop/jingyan_go_exer/mininet/create_tree_topo.sh"
+
+var MininetExecCmd = []string{"/bin/bash", "create_tree_topo.sh"}
+
+func NewClientWithOpts() *client.Client {
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		log.Print("The initialization of API client for docker daemon failed as the following reason: ")
-		panic(err)
+		log.Printf("Initializing API client for docker daemon failed as: %v", err)
+		os.Exit(1)
 	}
+	return cli
+}
 
-	//
-	// Pull "mininet" and "onos" images from docker hub
-	// Print err msg and exit if the operation fails
-	//
-	image_list := []string{"registry.hub.docker.com/iwaseyusuke/mininet", "registry.hub.docker.com/onosproject/onos"}
-	for _, image := range image_list {
-		_, err := cli.ImagePull(context.Background(), image, types.ImagePullOptions{})
-		if err != nil {
-			log.Print("Pull docker image" + image + "failed as the following reason: ")
-			panic(err)
-		}
-	}
-
-	//
-	// Create a container named "onos" based on the given "&container.Config" and "&container.HostConfig" configuration
-	// The "&container.Config" and "&container.HostConfig" are mainly used for port mapping here
-	// Print err msg and exit if the operation fails
-	//
-	onos_resp, err := cli.ContainerCreate(context.Background(),
-		&container.Config{
-			Image: "registry.hub.docker.com/onosproject/onos",
-			ExposedPorts: nat.PortSet{
-				"8181/tcp": struct{}{},
-				"8101/tcp": struct{}{},
-				"5005/tcp": struct{}{},
-				"830/tcp":  struct{}{}},
-		},
-		&container.HostConfig{
-			PortBindings: nat.PortMap{
-				"8181/tcp": []nat.PortBinding{{HostPort: "8181"}},
-				"8101/tcp": []nat.PortBinding{{HostPort: "8101"}},
-				"5005/tcp": []nat.PortBinding{{HostPort: "5005"}},
-				"830/tcp":  []nat.PortBinding{{HostPort: "830"}}},
-		}, nil, nil, "onos")
+func ImagePull(cli *client.Client, imageName string) {
+	_, err := cli.ImagePull(context.Background(), imageName, types.ImagePullOptions{})
 	if err != nil {
-		log.Print("Creating ONOS contianer failed as the following reason: ")
-		panic(err)
+		log.Printf("Pulling docker image %s failed as: %v", imageName, err)
+		os.Exit(1)
 	}
+}
 
-	//
-	// Start the "onos" container based on the "onos_resp.ID" created above
-	// Print err msg and exit if the operation fails
-	//
-	if err := cli.ContainerStart(context.Background(), onos_resp.ID, types.ContainerStartOptions{}); err != nil {
-		log.Print("Starting ONOS container failed as the following reason: ")
-		panic(err)
-	}
-
-	//
-	// Create a container named "mininet" based on the given "&container.HostConfig" configuration
-	// The "&container.HostConfig" is mainly used for volumn mapping
-	// Print err msg and exit if the operation fails
-	//
-	mininet_resp, err := cli.ContainerCreate(context.Background(),
-		&container.Config{
-			Tty:   true,
-			Image: "registry.hub.docker.com/iwaseyusuke/mininet",
-		},
-		&container.HostConfig{
-			Privileged: true,
-			Mounts: []mount.Mount{
-				{
-					Type:   mount.TypeBind,
-					Source: "/Users/yanjing/Desktop/jingyan_go_exer/mininet",
-					Target: "/tmp",
-				},
-			},
-		}, nil, nil, "mininet")
+func ContainerStart(cli *client.Client, config *container.Config, hostConfig *container.HostConfig, containerName string) string {
+	resp, err := cli.ContainerCreate(context.Background(), config, hostConfig, nil, nil, containerName)
 	if err != nil {
-		log.Print("Creating mininet contianer failed as the following reason: ")
-		panic(err)
+		log.Printf("Creating %s container failed as: %v", containerName, err)
+		os.Exit(1)
 	}
 
-	//
-	// Start the "mininet" container based on the "mininet_resp.ID" created above
-	// Print err msg and exit if the operation fails
-	//
-	if err := cli.ContainerStart(context.Background(), mininet_resp.ID, types.ContainerStartOptions{}); err != nil {
-		log.Print("Starting mininet container failed as the following reason: ")
-		panic(err)
+	if err := cli.ContainerStart(context.Background(), resp.ID, types.ContainerStartOptions{}); err != nil {
+		log.Printf("Starting %s container failed as %v: ", resp.ID, err)
+		os.Exit(1)
 	}
 
-	//
-	// Wait for 30s until ONOS container is ready for accepting HTTP request
-	//
+	return resp.ID
+}
+
+func ActivateOnosApp(appName string) {
+	url := "http://127.0.0.1:8181/onos/v1/applications/" + appName + "/active"
+
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		log.Printf("HTTP Request establishment failed as: %v", err)
+		os.Exit(1)
+	}
+	req.SetBasicAuth("onos", "rocks")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("Sending HTTP Request failed as %v: ", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	httpStatusCode := resp.StatusCode
+	if httpStatusCode != 200 {
+		log.Printf("Activate the %s failed because of %d HTTP StatusCode", appName, httpStatusCode)
+		os.Exit(1)
+	}
+}
+
+func ContainerInspect(cli *client.Client, containerID string) string {
+	var OnosIP = ""
+	cli.ContainerInspect(context.Background(), containerID)
+
+	onosInspect, err := cli.ContainerInspect(context.Background(), containerID)
+	if err != nil {
+		log.Printf("Fialed to get the infomation of ONOS container as: %v", err)
+		os.Exit(1)
+	}
+
+	for _, network := range onosInspect.NetworkSettings.Networks {
+		OnosIP = network.IPAddress
+	}
+
+	return OnosIP
+}
+
+func GenerateTopologyCreationFile(OnosIP string) {
+	input, err := ioutil.ReadFile(TemplateTopologyFile)
+	if err != nil {
+		log.Printf("Reading the template file failed as: %v", err)
+		os.Exit(1)
+	}
+	output := bytes.Replace(input, []byte("onos_ip"), []byte(OnosIP), -1)
+
+	if err = ioutil.WriteFile(NewTopologyFile, output, 0666); err != nil {
+		log.Printf("Writing to the new file failed as: %v", err)
+		os.Exit(1)
+	}
+}
+
+func ContainerExecCmd(cli *client.Client, containerID string, mininetExecConfig types.ExecConfig) string {
+	headers, err := cli.ContainerExecCreate(context.Background(), containerID, mininetExecConfig)
+	if err != nil {
+		log.Printf("Failed to create the exec configuration to run the exec process as: %v", err)
+		os.Exit(1)
+	}
+
+	resp, err := cli.ContainerExecAttach(context.Background(), headers.ID, types.ExecStartCheck{})
+	if err != nil {
+		log.Printf("Failed to Attach the connection to the exec process in the server as: %v", err)
+		os.Exit(1)
+	}
+	defer resp.Close()
+
+	data, _ := ioutil.ReadAll(resp.Reader)
+	return string(data)
+}
+
+func main() {
+	cli := NewClientWithOpts()
+
+	imageList := []string{MininetImageName, OnosImageName}
+	for _, imageName := range imageList {
+		ImagePull(cli, imageName)
+	}
+
+	onosConfig, onosHostConfig := &container.Config{
+		Image: OnosImageName,
+		ExposedPorts: nat.PortSet{
+			"8181/tcp": struct{}{},
+			"8101/tcp": struct{}{},
+			"5005/tcp": struct{}{},
+			"830/tcp":  struct{}{}},
+	}, &container.HostConfig{
+		PortBindings: nat.PortMap{
+			"8181/tcp": []nat.PortBinding{{HostPort: "8181"}},
+			"8101/tcp": []nat.PortBinding{{HostPort: "8101"}},
+			"5005/tcp": []nat.PortBinding{{HostPort: "5005"}},
+			"830/tcp":  []nat.PortBinding{{HostPort: "830"}}},
+	}
+
+	var onosID = ContainerStart(cli, onosConfig, onosHostConfig, OnosContainerName)
+
+	mininetConfig, mininetHostConfig := &container.Config{
+		Tty:   true,
+		Image: MininetImageName,
+	}, &container.HostConfig{
+		Privileged: true,
+		Mounts: []mount.Mount{{
+			Type:   mount.TypeBind,
+			Source: HostVolMap,
+			Target: MininetVolMap,
+		}},
+	}
+
+	var mininetID = ContainerStart(cli, mininetConfig, mininetHostConfig, MininetContainerName)
+
 	time.Sleep(30 * time.Second)
 
-	//
-	// Call "func Activate_onos_app(app_name string) int" to activate necessary applications for ONOS container
-	// Print err msg and exit if the operation fails
-	//
-	onos_apps_list := []string{"org.onosproject.fwd", "org.onosproject.openflow"}
-	for _, onos_app := range onos_apps_list {
-		resp_StatusCode := activate_onos_app.Activate_onos_app(onos_app)
-		if resp_StatusCode != 200 {
-			panic("Activate the " + onos_app + " failed because of " + string(rune(resp_StatusCode)) + " HTTP StatusCode")
-		}
+	onosAppsList := []string{OnosFwdAppName, OnosOpenflowName}
+	for _, appName := range onosAppsList {
+		ActivateOnosApp(appName)
 	}
 
-	//
-	// Obtain the IP address of ONOS container and store the value in the Onos_ip variable
-	//
-	var Onos_ip = ""
+	var onosIP = ContainerInspect(cli, onosID)
+	GenerateTopologyCreationFile(onosIP)
 
-	onos_inspect, err := cli.ContainerInspect(context.Background(), onos_resp.ID)
-	if err != nil {
-		log.Print("Fialed to get the infomation of ONOS container as the following reason: ")
-		panic(err)
-	}
-
-	for _, network := range onos_inspect.NetworkSettings.Networks {
-		Onos_ip = network.IPAddress
-	}
-
-	generate_file4topo_create.Generate_file4topo_create(Onos_ip)
-
-	// Execute the "/bin/bash /tmp/create_tree_topo.sh" in the mininet container
-
-	//
-	// Create a exec configuration to run an exec process which is specified in the "types.ExecConfig"
-	//
-	idre, err := cli.ContainerExecCreate(context.Background(), mininet_resp.ID, types.ExecConfig{
+	mininetExecConfig := types.ExecConfig{
 		Privileged:   true,
 		Tty:          true,
 		AttachStderr: true,
 		AttachStdout: true,
-		WorkingDir:   "/tmp",
-		Cmd:          []string{"/bin/bash", "create_tree_topo.sh"},
-	})
-	if err != nil {
-		log.Print("Failed to create the exec configuration to run the exec process as the following reason: ")
-		panic(err)
+		Cmd:          MininetExecCmd,
 	}
 
-	//
-	// Attach the connection to the exec process in the server
-	//
-	hijare, err := cli.ContainerExecAttach(context.Background(), idre.ID, types.ExecStartCheck{})
-	if err != nil {
-		log.Print("Failed to Attach the connection to the exec process in the server as the following reason: ")
-		panic(err)
-	}
-	defer hijare.Close()
-
-	//
-	// Output of the command "/bin/bash /tmp/create_tree_topo.sh"
-	//
-	data, _ := ioutil.ReadAll(hijare.Reader)
-	fmt.Println(string(data))
+	ContainerExecCmd(cli, mininetID, mininetExecConfig)
 }
